30e7c13cbcc70824256e2a3da620d68925d10975
jarvis73 <zjw.math@qq.com>
2019-05-26 22:19:55 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# BubbleNets
+Learning to Select the Guidance Frame in Video Object Segmentation by Deep Sorting Frames
+
+Contact: Brent Griffin (griffb at umich dot edu)
+
+## Change Logs
+
+* This repository is forked from [griffbr/BubbleNets](https://github.com/griffbr/BubbleNets)
+
+* Update to `Python3`
+
+* Format code style
+
+## Some results 
+
+* soapbox
+
+| frame | annotate frame 0 (OSVOS) | annotate frame 45(BubbleNets) |
+|:---:|:---:|:---:|
+|10|![img](./images/00_10.png)|![img](./images/45_10.png)|
+|30|![img](./images/00_30.png)|![img](./images/45_30.png)|
+|50|![img](./images/00_50.png)|![img](./images/45_50.png)|
+|70|![img](./images/00_70.png)|![img](./images/45_70.png)|
+|90|![img](./images/00_90.png)|![img](./images/45_90.png)|
+
+
+## Reference
+[BubbleNets: Learning to Select the Guidance Frame in Video Object Segmentation by Deep Sorting Frames](http://arxiv.org/abs/1903.11779 "arXiv Paper")<br />
+[Brent A. Griffin](https://www.griffb.com) and [Jason J. Corso](http://web.eecs.umich.edu/~jjcorso/)<br />
+IEEE Conference on Computer Vision and Pattern Recognition (CVPR), 2019
+
+Please cite our paper if you find it useful for your research.
+```
+@inproceedings{GrCoCVPR19,
+  author = {Griffin, Brent A. and Corso, Jason J.},
+  booktitle={2019 IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
+  title = {BubbleNets: Learning to Select the Guidance Frame in Video Object Segmentation by Deep Sorting Frames},
+  year = {2019}
+}
+```
---
+++ bubblenets_select_frame.py
@@ -0,0 +1,134 @@
+# Quickly annotate images using BubbleNets suggested frames.
+# Brent Griffin, 181127
+# Questions? griffb@umich.edu
+import cv2
+import os
+import glob
+
+from methods.preprocess.grabCutClass import GrabCutter
+from methods.preprocess.videoProcessor import VideoProcessor
+from methods.annotate_suggest.annotation_suggester import annotation_suggester
+from methods.annotate_suggest.color_hist_frame_select import color_hist_frame_select
+from methods.annotate_suggest.ResNet_preprocess import resnet_process_data_dir
+from methods.annotate_suggest.BubbleNets_frame_select import BubbleNets_sort
+from methods.annotate_suggest.BubbleNets import bn_utils
+
+user_scale = True
+user_select = True
+
+
+def get_user_annotation(videoDir):
+    print('\n\nGenerating user-guided annotation for ' + os.path.basename(videoDir) + '.\n')
+    annotationDir = os.path.join(videoDir, 'usrAnnotate')
+    if not os.path.isdir(annotationDir):
+        os.makedirs(annotationDir)
+    imageDir = os.path.join(videoDir, 'src')
+    imageFiles = glob.glob(os.path.join(imageDir, '*'))
+    imageFiles.sort()
+    # Get list of suggested annotations.
+    ant_idx, ant_file = read_annotation_list(os.path.join(videoDir, 'frame_selection', 'all.txt'))
+    # Run automated annotation suggestion (just to get folder info).
+    suggester = annotation_suggester(videoDir)
+    userAnnotating = True
+    while userAnnotating:
+        antImageFiles = glob.glob(os.path.join(annotationDir, '*'))
+        nAntImgs = len(antImageFiles)
+        print('Currently ' + str(nAntImgs) + ' annotation image(s):')
+        for i in range(0, nAntImgs):
+            ant_name = os.path.basename(antImageFiles[i])
+            print(ant_name)
+            if ant_name in ant_file:
+                del ant_file[ant_file.index(ant_name)]
+        print('Suggested annotation frames remaining:')
+        print(ant_file)
+        # Ask if there are any other images they would like to annotate?
+        response = input('Annotate another image? (y or n)\n')
+        if response not in {'y', 'Y', 'Yes', 'yes'}:
+            userAnnotating = False
+        if userAnnotating:
+            # User annotation specific image.
+            while True:
+                if user_select:
+                    annotationImageIdx = int(input('What is preferred annotation image index? (' + str(
+                        os.path.basename(imageFiles[0])) + '-' + str(
+                        os.path.basename(imageFiles[-1])) + ' possible)\n'))
+                    annotationImageIdx -= suggester.manip_start_idx
+                else:
+                    annotationImageIdx = ant_idx[0]
+                try:
+                    imageDir = imageFiles[int(annotationImageIdx)]
+                    annotationImage = cv2.imread(imageDir)
+                    windowx = 100
+                    windowy = 100
+                    if user_scale:
+                        cv2.imshow('Annotation Image', annotationImage)
+                        cv2.moveWindow('Annotation Image', windowx, windowy)
+                        cv2.waitKey(20)
+                        scale = float(input('What is preferred scale? (e.g., 1, 2, or 0.5)\n'))
+                    else:
+                        scale = 1
+                    annotationImageScaled = cv2.resize(annotationImage, (0, 0), fx=scale, fy=scale)
+                    cv2.imshow('Scaled Annotation Image', annotationImageScaled)
+                    cv2.moveWindow('Scaled Annotation Image', windowx, windowy)
+                    cv2.waitKey(20)
+                    response = input('Is annotation frame acceptable? (y or n)\n')
+                    if response in {'y', 'Y', 'Yes', 'yes'}:
+                        cv2.destroyAllWindows()
+                        # cv2.waitKey()
+                        break
+                except:
+                    print('Image ' + str(annotationImageIdx) + ' does not exist!')
+            outputMaskDir = os.path.join(annotationDir, os.path.basename(imageDir).split('.')[0] + '.png')
+            # Let user annotate selected image.
+            GrabCutter(imageDir, outputMaskDir, windowx, windowy, scale)
+            save_extra_image_copy(imageDir, videoDir, nAntImgs)
+
+
+def save_extra_image_copy(image_dir, video_dir, annotation_frame_num):
+    # TODO: add visualization for mask on top of image.
+    print('Saving extra copy of annotation image for development.')
+    extra_image_dir = os.path.join(video_dir, 'annotation_imgs')
+    if not os.path.isdir(extra_image_dir):
+        os.makedirs(extra_image_dir)
+    cv2.imwrite(os.path.join(extra_image_dir, format(annotation_frame_num, '02d')
+                             + '_annotation_' + os.path.basename(image_dir).split('.')[0] + '.jpg'),
+                cv2.imread(image_dir))
+
+
+def read_annotation_list(text_file):
+    read_list = bn_utils.read_list_file(text_file)
+    n_ant = int(read_list[0].split(' ')[0])
+    ant_idx = []
+    ant_file = []
+    for i in range(n_ant):
+        ant_idx.append(int(read_list[i * 2 + 1]))
+        ant_file.append(read_list[i * 2 + 2])
+    return ant_idx, ant_file
+
+
+def main():
+    mainDir = os.getcwd()
+    dataDir = os.path.join(mainDir, 'data')
+    rawDataDir = os.path.join(dataDir, 'rawData')
+    # Get list of video directories.
+    videoList = sorted(next(os.walk(rawDataDir))[1])
+    # Preprocess through ResNet.
+    resnet_process_data_dir(rawDataDir)
+    # Get BubbleNets suggested annotated frame.
+    BubbleNets_sort(rawDataDir, model='BNLF')
+    BubbleNets_sort(rawDataDir, model='BN0')
+    color_hist_frame_select(rawDataDir, annotate_rate=int(10e6))
+    # Cycle through each video.
+    for i, videoName in enumerate(videoList):
+        # Misc. setup.
+        videoDir = os.path.join(rawDataDir, videoName)
+        # Check for src folder. If it doesn't exist, make it from video.
+        VideoProcessor(videoDir)
+        # Check for annotation files, if not there, ask users which frames they would like annotated.
+        get_user_annotation(videoDir)
+        print('Finished with ' + videoName + ' annotation.\n\n')
+    print('\n\nFinished with all annotations!\n\n')
+
+
+if __name__ == "__main__":
+    main()
